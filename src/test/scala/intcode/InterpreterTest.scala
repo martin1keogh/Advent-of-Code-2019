3ba3eda323be4b5591c0355ceb898b1cc967d135
Martin Keogh <martin1keogh@gmail.com>
2019-12-16 13:25:13 +0100

    refacto: rework InterpreterTest test organization

--- src/test/scala/intcode/InterpreterTest.scala
+++ src/test/scala/intcode/InterpreterTest.scala
@@ -1,34 +1,49 @@
 package intcode
 
-import common.AoCExampleRunner
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
 import scala.collection.immutable.SeqMap
 
-class InterpreterTest extends AoCExampleRunner with Interpreter {
+class InterpreterTest extends AnyWordSpec with Matchers with Interpreter {
   type Input = Array[Cell]
-  override type Output = Array[Cell]
+  type Output = Array[Cell]
 
-  val examples: SeqMap[Input, Output] = SeqMap(
-    // day 2 related examples
-    "1,0,0,0,99" -> "2,0,0,0,99",
-    "2,3,0,3,99" -> "2,3,0,6,99",
-    "2,4,4,5,99,0" -> "2,4,4,5,99,9801",
-    "1,1,1,4,99,5,6,0,99" -> "30,1,1,4,2,5,6,0,99",
+  val examples: SeqMap[String, Seq[(Input, Output)]] = SeqMap(
+    "day 2" -> Seq(
+      "1,0,0,0,99" -> "2,0,0,0,99",
+      "2,3,0,3,99" -> "2,3,0,6,99",
+      "2,4,4,5,99,0" -> "2,4,4,5,99,9801",
+      "1,1,1,4,99,5,6,0,99" -> "30,1,1,4,2,5,6,0,99",
+    ),
 
-    // day 5
-    "1002,4,3,4,33" -> "1002,4,3,4,99",
-    "1101,100,-1,4,0" -> "1101,100,-1,4,99",
-    "4,3,99,0" -> "4,3,99,0",
+    "day 5" -> Seq(
+      "1002,4,3,4,33" -> "1002,4,3,4,99",
+      "1101,100,-1,4,0" -> "1101,100,-1,4,99",
+      "4,3,99,0" -> "4,3,99,0",
+    ),
 
     // test Write() accepts mode
-    "104,0,99" -> "104,0,99",
-  ).map { case (input, output) =>
+    "write instruction modes" -> Seq(
+      "104,0,99" -> "104,0,99",
+    )
+  ).view.mapValues(_.map { case (input, output) =>
     input.split(",").map(s => Cell(s.toInt)) -> output.split(",").map(s => Cell(s.toInt))
-  }
+  }).to(SeqMap)
 
   val method: Input => Output = { input =>
     runner.runS(Program(input)).value.underlying
   }
 
-  "process" should { behave like examplesSolver() }
+  "Interpreter#runner" should afterWord("validate") {
+    examples.foreach { case (exampleGroup, examples) =>
+      exampleGroup should {
+        examples.zipWithIndex.foreach { case ((input, output), index) =>
+          s"example number ${index + 1}" in {
+            method(input) should be(output)
+          }
+        }
+      }
+    }
+  }
 }
